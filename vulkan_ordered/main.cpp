ec91010d535ae107edf0a51544118dd1bd24acc1
ochHerrje <och.herrje@gmail.com>
2021-07-29 15:11:47 +0200

    Somewhat adjusted font testing.

--- vulkan_ordered/main.cpp
+++ vulkan_ordered/main.cpp
@@ -20,10 +20,7 @@
 
 #include "sdf_image.h"
 
-#include "opentype_file.h"
-
-#include <cstdio>
-#include <Windows.h>
+#include "truetype.h"
 
 enum class sample_type
 {
@@ -51,88 +48,58 @@ och::err_info image_testing() noexcept
 	return {};
 }
 
-och::err_info write_glyph_info(const opentype_file& file, char32_t codept)
+void write_glyph_info(const truetype_file& file, uint32_t codept, och::stringview font_name, bool& has_erred_before)
 {
 	glyph_data glyph = file.get_glyph(codept);
 
+	if (!has_erred_before)
+	{
+		has_erred_before = true;
+
+		och::print("\n\nFont: {}\n\n", font_name);
+	}
+
 	och::print(
-		"'{}':\n\n"
-		"x_min: {}\n"
-		"x_max: {}\n"
-		"y_min: {}\n"
-		"y_max: {}\n"
-		"advance_width: {}\n"
-		"left_side_bearing: {}\n"
-		"contours : {}\n"
-		"points : {}\n\n",
+		"0x{:4>~0X}: advance_width: {}; left_side_bearing: {}; contours : {}; points : {}\n",
 		codept,
-		glyph.x_min(),
-		glyph.x_max(),
-		glyph.y_min(),
-		glyph.y_max(),
-		glyph.advance_width(),
-		glyph.left_side_bearing(),
+		glyph.metrics().advance_width(),
+		glyph.metrics().left_side_bearing(),
 		glyph.contour_cnt(),
 		glyph.point_cnt());
-
-	char bmp_name_buf[256];
-
-	och::sprint(bmp_name_buf, "textures/codept_{:4>~0X}_.bmp", static_cast<uint32_t>(codept));
-
-	constexpr uint32_t bmp_sz_x = 256, bmp_sz_y = 256;
-
-	bitmap_file glyph_bmp(bmp_name_buf, och::fio::open_truncate, bmp_sz_x, bmp_sz_y);
-
-	if (!glyph_bmp)
-		return MSG_ERROR("Could not open bmp for drawing font");
-
-	glyph_bmp.point_op([](texel_b8g8r8) noexcept -> texel_b8g8r8 {return { 0, 0, 0 }; });
-
-	for (uint32_t i = 0; i != glyph.contour_cnt(); ++i)
-	{
-		glyph_contour c = glyph.get_contour(i);
-
-		och::print("\nCONTOUR {} ({} points):\n", i, c.point_cnt());
-
-		for (uint32_t j = 0; j != c.point_cnt(); ++j)
-		{
-			glyph_point p = c[j];
-
-			och::print("{:3>}:   ({},{})\n", j, p.x, p.y);
-		}
-	}
-
-	for (uint16_t i = 0; i != glyph.point_cnt(); ++i)
-	{
-		glyph_point p = glyph.get_point(i);
-		
-		int32_t px = static_cast<int32_t>(p.x * bmp_sz_x);
-		int32_t py = static_cast<int32_t>(p.y * bmp_sz_y);
-
-		if (px < 0)
-			px = 0;
-		
-		if (py < 0)
-			py = 0;
-
-		glyph_bmp(px, py) = glyph.is_point_on_curve(i) ? texel_b8g8r8(0x00, 0x7F, 0xFF) : texel_b8g8r8(0xD0, 0x7F, 0x00);
-	}
-
-	och::print("\n\n");
-
-	return {};
 }
 
 och::err_info font_testing() noexcept
 {
-	opentype_file file("C:\\Windows\\Fonts\\calibri.ttf");
+	och::file_search folder("C:/Windows/Fonts", och::fio::search_for_files, "?ttf");
 
-	if (!file)
-		return MSG_ERROR("Could not open file");
+	och::print("Starting search...\n\n");
 
-	check(write_glyph_info(file, U'ǻ'));
+	uint32_t file_cnt = 0;
 
-	//write_glyph_info(file, U'ü');
+	for (auto file_info : folder)
+	{
+		och::stringview ending = file_info.ending();
+
+		++file_cnt;
+
+		truetype_file file(file_info.absolute_name().raw_cbegin());
+
+		if (!file)
+		{
+			och::print("\nError with file {}\n", file_info.absolute_name());
+
+			continue;
+		}
+
+		och::print("{}\n", file_info.name());
+
+		//bool has_erred_before = false;
+		//
+		//for (uint32_t i = 0; i != 0x10000; ++i)
+		//	write_glyph_info(file, i, file_info.name(), has_erred_before);
+	}
+
+	och::print("\n{} Files found\n", file_cnt);
 
 	return {};
 }
