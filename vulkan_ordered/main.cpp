27af4d5a33bea9e9b01fc847ee3350a98d048ac7
ochHerrje <och.herrje@gmail.com>
2021-09-24 14:07:45 +0200

    Added additional font testing

--- vulkan_ordered/main.cpp
+++ vulkan_ordered/main.cpp
@@ -37,38 +37,94 @@ enum class sample_type
 
 och::err_info font_testing() noexcept
 {
-	truetype_file ttf("C:/Windows/Fonts/consola.ttf");
-	
-	if (!ttf)
-		return MSG_ERROR("Could not open ttf file");
-	
+	//consola.ttf
+	#define FONT_NAME "ALGER"
+
+	const char* ttf_filename = "C:/Windows/Fonts/" FONT_NAME ".ttf"; // "C:/Windows/Fonts/consola.ttf";
+	const char* glfatl_filename = "C:/Users/alex_2/source/repos/vulkan_ordered/vulkan_ordered/textures/" FONT_NAME ".glfatl";
+
+	{
+		truetype_file ttf_file(ttf_filename);
+
+		glyph_data glf = ttf_file.get_glyph_data_from_codepoint('y');
+
+		sdf_image img;
+
+		img.from_glyph(glf, 64, 64, 0.75F);
+
+		img.save_bmp("C:/Users/alex_2/source/repos/vulkan_ordered/vulkan_ordered/textures/glyph_sdf.bmp", true, sdf_image::colour_mapper::nonlinear_distance);
+
+		och::print("width: {}\nheight: {}\n", glf.metrics().x_size(), glf.metrics().y_size());
+	}
 
 
-	codept_range ranges[1]{ {0, 1024} }; // { {32, 95} };
-	
+
 	glyph_atlas atlas;
+
+	if (atlas.load_glfatl(glfatl_filename))
+	{
+		och::print("Could not find existing glyph atlas \"{}\".\nCreating new file from \"{}\".\n", glfatl_filename, ttf_filename);
+
+		codept_range ranges[1]{ {32, 95} };
+
+		constexpr float clamp = 0.015625F * 2.0F;
+
+		check(atlas.create(ttf_filename, 64, 2, clamp, 1024, och::range(ranges)));
+
+		check(atlas.save_glfatl(glfatl_filename, true));
+
+		check(atlas.save_bmp("textures/atlas_ttfdirect.bmp", true));
+	}
+	else
+	{
+		och::print("Existing glyph atlas found.\n");
+
+		check(atlas.save_bmp("textures/atlas_readback.bmp", true));
+
+		och::print("Loaded existing glyph atlas from {}\n", glfatl_filename);
+	}
+
+	constexpr uint32_t text_w = 2048;
+
+	bitmap_file text_bmp("textures/text.bmp", och::fio::open_truncate, text_w, 256);
+
+	image_view atlas_view{ atlas.view() };
+
+	uint32_t curr_x = 32;
+
+	const uint32_t line_y = 64;
 	
-	constexpr float clamp = 0.015625F * 2.0F;
-	
-	check(atlas.create("C:/Windows/Fonts/consola.ttf", 128, 2, clamp, 1024, och::range(ranges)));
-	
-	check(atlas.save_bmp("textures/atlas.bmp", true));
-	
+	uint8_t c = static_cast<uint8_t>(getchar());
+
+	while (c != '\n')
+	{
+		glyph_atlas::glyph_index idx = atlas(c);
+
+		const uint32_t x_lo = static_cast<uint32_t>(idx.position.x * atlas.width());
+
+		const uint32_t y_lo = static_cast<uint32_t>(idx.position.y * atlas.height());
+
+		const uint32_t x_sz = static_cast<uint32_t>(idx.size.x * atlas.width() + 2.0F);
+
+		const uint32_t y_sz = static_cast<uint32_t>(idx.size.y * atlas.height() + 2.0F);
+
+		const uint32_t brg_x = static_cast<uint32_t>(idx.bearing.x * atlas.width());
+
+		const uint32_t brg_y = static_cast<uint32_t>(idx.bearing.y * atlas.height());
+
+		och::print("U+{:4>~0X} ({}): brg_x: {} -> {}\n           brg_y: {} -> {}\n\n", static_cast<uint32_t>(c), static_cast<char>(c), idx.bearing.x, static_cast<int32_t>(brg_x), idx.bearing.y, static_cast<int32_t>(brg_y));
+
+		for (uint32_t y = 0; y != y_sz; ++y)
+			for (uint32_t x = 0; x != x_sz; ++x)
+				if (const uint8_t v = atlas_view(x_lo + x, y_lo + y))
+					text_bmp(curr_x + brg_x + x, line_y + brg_y + y) = texel_b8g8r8(v, v, v);
+
+		curr_x += static_cast<uint32_t>(idx.advance * atlas.width() + 0.99F);
+
+		c = static_cast<uint8_t>(getchar());
+	}
+
 	atlas.destroy();
-
-
-
-	//glyph_data glyph = ttf.get_glyph_data_from_id(591); // 165 -> { 131, 333, 319 }
-	//
-	//glyph_data glyph = ttf.get_glyph_data_from_codepoint(U'A');
-	//
-	//sdf_image img;
-	//
-	//check(img.from_glyph(glyph, 64, 64, 0.75F));
-	//
-	//check(img.save_bmp("textures/glyph.bmp", true, sdf_image::colour_mapper::monochrome));
-
-
 
 	return {};
 }
@@ -82,7 +138,7 @@ och::err_info testing() noexcept
 
 int main()
 {
-	constexpr sample_type to_run = sample_type::testing;
+	constexpr sample_type to_run = sample_type::compute_simplex_to_swapchain;
 
 	och::err_info err{};
 
